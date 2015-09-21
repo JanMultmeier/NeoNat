339a462e0dafe0d39ff47f7b7048bf41514ec104
JanMultmeier <Jan.Multmeier@googlemail.com>
2015-09-21 08:59:34 +0200

    Initial Commit

---
+++ R/BornBy.R
@@ -0,0 +1,28 @@
+#' Calculating Date of Birth from the First Day of the Last Menstrual Cycle
+#'
+#' @param x a vector, first day of the last menstrual cycle
+#' @param format a text string explicating the date format for x, defaults to "%d.%m.%Y"
+#' @param get either "point" or "interval" for just the point estimate or the interval boundaries, using a standard deviation of 13 days, respectively
+#' @param sd indicates the width of the uncertainty interval as multiples of the standard deviation (13 days)
+#' @return a vector of estimated dates of birth (for "point") or a data.frame of dates of birth and lower and upper boundaries
+#' @keywords birthday naegele
+#' @export
+#' @examples
+#' BornBy("31.12.1984")
+#' BornBy(c("11.11.1984","08.07.1990"),get="interval",sd=1)
+
+BornBy <- function(x,format="%d.%m.%Y",get="point",sd=1.96) {
+  MenDate <- as.Date(x,format=format)
+  birthDate <- MenDate + 280
+  i_low <- birthDate - sd*13
+  i_high <- birthDate + sd*13
+  
+  if (get=="point"){
+    output <- birthDate
+  }
+  if (get=="interval"){
+    output <- data.frame(birthDate,i_low,i_high)
+    colnames(output) <- c("Date of Birth","lower estimate","upper estimate")
+  }
+  return(output)
+}
---
+++ R/MadeOn.R
@@ -0,0 +1,28 @@
+#' Calculating Date of Conception from Date of Birth
+#'
+#' @param x a vector of birth dates
+#' @param format a text string explicating the date format for x, defaults to "%d.%m.%Y"
+#' @param get either "point" or "interval" for just the point estimate or the interval boundaries, using a standard deviation of 13 days, respectively
+#' @param sd indicates the width of the uncertainty interval as multiples of the standard deviation (13 days)
+#' @return a vector of estimated dates of conception (for "point") or a data.frame of dates of conception and lower and upper boundaries
+#' @keywords birthday conception naegele
+#' @export
+#' @examples
+#' MadeOn("31.05.1984")
+#' MadeOn(c("24.04.1984","01.12.1983"),get="interval",sd=1)
+
+MadeOn <- function(x,format="%d.%m.%Y",get="point",sd=1.96) {
+  birthDate <- as.Date(x,format=format)
+  conceptionDate <- birthDate - 266
+  i_low <- conceptionDate - sd*13
+  i_high <- conceptionDate + sd*13
+  
+  if (get=="point"){
+    output <- conceptionDate
+  }
+  if (get=="interval"){
+    output <- data.frame(conceptionDate,i_low,i_high)
+    colnames(output) <- c("Date of Conception","lower estimate","upper estimate")
+  }
+  return(output)
+}
